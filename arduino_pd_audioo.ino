1d19577cd09119e28c1e649f86f9c97ef204feb2
Fatlys <143842013+Fatlys@users.noreply.github.com>
2026-09-23 13:56:09 -0400

    sans hold

--- arduino_pd_audioo.ino
+++ arduino_pd_audioo.ino
@@ -61,4 +61,68 @@ void loop() {
   }
  
 }
+
+
+
+#include <Arduino.h>
+#include <Bounce2.h>
+ 
+#define TOGGLE_BUTTON_PIN 2
+#define TOGGLE_LIGHT_PIN 3
+#define TOGGLE_PREFIX "bouton0"
+ 
+#define MOMENTARY_BUTTON_PIN 4
+#define MOMENTARY_LIGHT_PIN 5
+#define MOMENTARY_PREFIX "bouton1"
+ 
+#define BAUD 115200
+ 
+Bounce2::Button toggleButton = Bounce2::Button();
+Bounce2::Button momentaryButton = Bounce2::Button();
+ 
+void setup() {
+  // LED config
+  pinMode(TOGGLE_LIGHT_PIN, OUTPUT);
+  digitalWrite(TOGGLE_LIGHT_PIN, LOW);
+  pinMode(MOMENTARY_LIGHT_PIN, OUTPUT);
+  digitalWrite(MOMENTARY_LIGHT_PIN, LOW);
+ 
+  // button config
+  toggleButton.attach(TOGGLE_BUTTON_PIN, INPUT_PULLUP);
+  toggleButton.setPressedState(LOW);
+ 
+  momentaryButton.attach(MOMENTARY_BUTTON_PIN, INPUT_PULLUP);
+  momentaryButton.setPressedState(LOW);
+ 
+  // serial config
+  Serial.begin(BAUD);
+}
+ 
+void loop() {
+  toggleButton.update();
+  momentaryButton.update();
+ 
+  if (toggleButton.pressed() || toggleButton.released()) {
+    bool pressed = toggleButton.isPressed();
+ 
+    digitalWrite(TOGGLE_LIGHT_PIN, pressed);
+ 
+    if(pressed) {
+        Serial.print(TOGGLE_PREFIX);
+        Serial.print(" 1\n");
+    }
+  }
+ 
+  if (momentaryButton.pressed() || momentaryButton.released()) {
+    bool pressed = momentaryButton.isPressed();
+ 
+    digitalWrite(MOMENTARY_LIGHT_PIN, pressed);
+ 
+    if(pressed) {
+        Serial.print(MOMENTARY_PREFIX);
+        Serial.print(" 1\n");
+    }
+  }
+ 
+}
  
